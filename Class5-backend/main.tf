da7ab2d8969879305daeac201e5a1c5770a0ea3a
tamyona <kazkadyrbek@gmail.com>
2025-01-23 03:06:43 +0000

    Class5

---
+++ Class5-backend/main.tf
@@ -0,0 +1,18 @@
+provider aws {
+    region = "us-east-2"
+}
+
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "deployer-key"
+  public_key = file("~/.ssh/id_rsa.pub")
+
+  tags = {
+    Team = "DevOps"
+    Environment = "Dev"
+    Owner = "Kadyrbek"
+    Owner2 = "Kadyrbek2"
+  }
+}
+
+
